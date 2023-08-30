e4b138d43be13986f998d45641dd71bed0681213
Nitu Dubey <nitutiwari01@gmail.com>
2023-08-29 21:22:53 -0400

    corrected the error

--- src/components/App.js
+++ src/components/App.js
@@ -1,71 +1,128 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+  });
+  const [selectedOption, setSelectedOption] = useState("Male");
 
-const [user, setUser]= useState({name:"", email:"", phoneNumber:"", password:"", })
-const [selectedOption, setSelectedOption] = useState("male")
-console.log(selectedOption)
-const [error, setError] = useState("")
+  const [error, setError] = useState("");
+  const [succes, setSucces] = useState("");
 
-function handleSubmitBtn(){
-  if(!user.name || !user.email || !user.password || !user.phoneNumber){
-    setError("All fields are mandatory")
-    return
+  function handleSubmitBtn() {
+    if (!user.name || !user.email || !user.password || !user.phoneNumber) {
+      setError("All fields are mandatory");
+      setSucces("")
+      return;
+    }
+    if (!/^[a-z0-9 ]+$/i.test(user.name)) {
+      setError("Name is not alphanumeric.");
+      setSucces("")
+      return;
+    }
+    if (!user.email.includes("@")) {
+      setError("Email must contain @.");
+      setSucces("")
+      return;
+    }
+    console.log(selectedOption !== "Male");
+    console.log(selectedOption != "female");
+    if (
+      ["Male", "female", "other"].indexOf(selectedOption) < 0
+      // selectedOption != "Male" ||
+      // selectedOption !== "female" ||
+      // selectedOption !== "other"
+    ) {
+      setError("Please identify as male, female or others.");
+      setSucces("")
+      return;
+    }
+    if (!/^[0-9]+$/.test(user.phoneNumber)) {
+      setError("Phone Number must contain only numbers.");
+      setSucces("")
+      return;
+    }
+    if (user.password.length < 6) {
+      setError("Password must contain atleast 6 letters");
+      setSucces("")
+      return;
+    }
+    let name = user.email.split("@")
+    setSucces("Hello " + name)
+    setError("")
+    
   }
-  if(!(/^[a-z0-9 ]+$/i.test(user.name))){
-    setError("Name is not alphanumeric.")
-    return
-  }
-  if(!user.email.includes("@")){
-    setError("Email must contain @.")
-    return
-  }
-  if(selectedOption !== "male" || selectedOption!== "female" || selectedOption!== "other"){
-    setError("Please identify as male, female or others.")
-    return
-  }
-  if(!(/^[0-9]+$/.test(user.phoneNumber)) ){
-    setError("Phone Number must contain only numbers.")
-    return
-  }
-  if(user.password.length > 6){
-    setError("Password must contain atleast 6 letters")
-    return
-  }
-  
-}
-
 
   return (
     <div id="main">
+      <label>Name</label>
+      <input
+        type="text"
+        placeholder="Enter Name"
+        value={user.name}
+        onChange={(e) => setUser({ ...user, name: e.target.value })}
+        data-testid="name"
+      />
+      <br />
+      <br />
 
-    <label >Name</label>
-      <input type="text" placeholder="Enter Name"   value={user.name} onChange={(e)=>setUser({...user, name:e.target.value})} data-testid = "name"/><br/><br/>
+      <label>Email</label>
+      <input
+        type="email"
+        placeholder="Enter Email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        data-testid="email"
+      />
+      <br />
+      <br />
 
-      <label >Email</label>
-      <input type="email" placeholder="Enter Email" value={user.email} onChange={(e)=>setUser({...user, email:e.target.value})} data-testid = "email"  /><br/><br/>
-
-      <label >Gender</label>
-      <select name="gender"  value={selectedOption} onChange={(e)=>setSelectedOption(e.target.value)} data-testid = "gender">
-        <option vlaue="">Select gender</option>
-        <option value="male" >male</option>
+      <label>Gender</label>
+      <select
+        name="gender"
+        value={selectedOption}
+        onChange={(e) => setSelectedOption(e.target.value)}
+        data-testid="gender"
+      >
+        <option value="">Select gender</option>
+        <option value="Male">Male</option>
         <option value="female">female</option>
         <option value="other">other</option>
-      </select><br/><br/>
+      </select>
+      <br />
+      <br />
 
       <label>Phone Number</label>
-      <input type="text" value={user.phoneNumber} onChange={(e)=>setUser({...user, phoneNumber:e.target.value})} data-testid ="phoneNumber" /><br/><br/>
+      <input
+        type="text"
+        value={user.phoneNumber}
+        onChange={(e) => setUser({ ...user, phoneNumber: e.target.value })}
+        data-testid="phoneNumber"
+      />
+      <br />
+      <br />
 
-      <label >Password</label>
-      <input type="password" value={user.password} onChange={(e)=>setUser({...user, password:e.target.value})} data-testid ="password"  /><br/><br/>
+      <label>Password</label>
+      <input
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        data-testid="password"
+      />
+      <br />
+      <br />
 
-      <button data-testid = "submit" onClick={handleSubmitBtn}>Submit button </button>
-      {error && <p>Error Message: {error}</p>}
-
+      <button data-testid="submit" onClick={handleSubmitBtn}>
+        Submit button{" "}
+      </button>
+      {error && <span>Error Message: {error}</span>}
+      {succes && <span>{succes}</span>}
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
